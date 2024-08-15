c8e19eb6459a3321cb2505d2f974be26ed3183ee
Jared York <jaredyork@Jareds-Laptop.local>
2024-08-15 12:18:43 -0400

    Changed the port number.

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ app.post('/process', { preHandler: upload.array('images', 1000) }, async (req, r
 });
 
 // Start the server
-const port = 8081;
+const port = 8082;
 app.listen({ port }, (err) => {
     if (err) {
         console.error(err);
